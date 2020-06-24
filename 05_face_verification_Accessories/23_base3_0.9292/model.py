a270e3fc4f8a72ff31dabf216e7d4cb730a78420
shiney5213 <shiney5213@gmail.com>
2020-06-24 19:28:56 +0900

    create files

---
+++ 05_face_verification_Accessories/23_base3_0.9292/model.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torch.nn.functional as F
+
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(1, 4, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(4),
+            
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(8, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(8*100*100, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 500),
+            nn.ReLU(inplace=True),
+
+            # 추가
+            
+            nn.Linear(500, 5))
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 64, 10),  # 64@96*96
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),  # 64@48*48
+            nn.Conv2d(64, 128, 7),
+            nn.ReLU(),    # 128@42*42
+            nn.MaxPool2d(2),   # 128@21*21
+            nn.Conv2d(128, 128, 4),
+            nn.ReLU(), # 128@18*18
+            nn.MaxPool2d(2), # 128@9*9
+            nn.Conv2d(128, 256, 4),
+            nn.ReLU(),   # 256@6*6
+        )
+        self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
+        self.out = nn.Linear(4096, 1)    
+
+        
+
+    def forward_once(self, x):
+        x = self.conv(x)
+        x = x.view(x.size()[0], -1)
+        output = self.liner(x)
+        return output
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2    
+
+    # def forward_once(self, x):
+    #     output = self.cnn1(x)
+    #     output = output.view(output.size()[0], -1)
+    #     output = self.fc1(output)
+    #     return output
+
+    # def forward(self, input1, input2):
+    #     output1 = self.forward_once(input1)
+    #     output2 = self.forward_once(input2)
+    #     return output1, output2
---
+++ 06_face_verification_Accessories_Limit/23_mobilefacenet_0.9119/dataloader.py
@@ -0,0 +1,105 @@
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+import torch
+from torch.utils import data
+import torchvision.transforms as transforms
+
+class CustomDataset(data.Dataset):
+    def __init__(self, root, phase = 'train', repeat_num = 10, transform=None,):
+        self.root = root
+        self.phase = phase
+        self.labels = {}
+        self.transform = transform
+        self.repeat_num = repeat_num
+        if self.phase != 'train':
+            self.label_path = os.path.join(root, self.phase, self.phase + '_label.csv')
+            # used to prepare the labels and images path
+            self.direc_df = pd.read_csv(self.label_path)
+            self.direc_df.columns = ["image1", "image2", "label"]
+            self.dir = os.path.join(root, self.phase)
+        else:
+            self.train_meta_dir = os.path.join(root, self.phase, self.phase + '_meta.csv')
+            train_meta = pd.read_csv(self.train_meta_dir)
+
+            train_data = []
+            # make_true_pair
+            id_list = list(set(train_meta['face_id']))  # people id
+            for i in range(int(self.repeat_num)):
+                for id in id_list:
+                    pair = []
+                    candidate = train_meta[train_meta['face_id'] == int(id)]
+                    pair.append(candidate[candidate['acc_option']=='none'].sample(1)['file_name'].item())
+                    pair.append(candidate[candidate['acc_option']=='acc'].sample(1)['file_name'].item())
+                    pair.append(0)
+                    train_data.append(pair)
+            # make_false_pair
+            id_list = list(set(train_meta['face_id']))
+            for i in range(int(self.repeat_num)):
+                for id in id_list:
+                    pair = []
+                    candidate = train_meta[train_meta['face_id'] == int(id)]
+                    candidate_others = train_meta[train_meta['face_id'] != int(id)]
+                    pair.append(candidate[candidate['acc_option']=='none'].sample(1)['file_name'].item())
+                    pair.append(candidate_others[candidate_others['acc_option']=='acc'].sample(1)['file_name'].item())
+                    pair.append(1)
+                    train_data.append(pair)
+            self.direc_df = pd.DataFrame(train_data)
+            self.direc_df.columns = ["image1", "image2", "label"]
+            self.dir = os.path.join(root, self.phase)
+            # self.direc_df.to_csv(os.path.join(root, self.phase, self.phase + '_label.csv'), mode='w', index=False)
+            # self.label_path = os.path.join(root, self.phase, self.phase + '_label.csv')
+            self.direc_df.to_csv(os.path.join(root, self.phase, f'{self.phase}_label_{repeat_num}.csv'), mode='w', index=False)
+            self.label_path = os.path.join(root, self.phase, f'{self.phase}_label_{repeat_num}.csv')
+            print(f'{self.phase}_label_{repeat_num}.csv 저장 완료')
+            
+
+    def __getitem__(self, index):
+        # getting the image path
+        image1_path = os.path.join(self.dir, self.direc_df.iat[index, 0])
+        image2_path = os.path.join(self.dir, self.direc_df.iat[index, 1])
+        # Loading the image
+        img0 = Image.open(image1_path)
+        img1 = Image.open(image2_path)
+        img0 = img0.convert("RGB")
+        img1 = img1.convert("RGB")
+        # img0 = img0.convert("L")
+        # img1 = img1.convert("L")
+
+        # Apply image transformations
+        if self.transform is not None:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+        if self.phase != 'test':
+            return (self.direc_df.iat[index, 0], img0, self.direc_df.iat[index, 1], img1,
+                    torch.from_numpy(np.array([int(self.direc_df.iat[index, 2])], dtype=np.float32)))
+        elif self.phase == 'test':
+            dummy = ""
+            return (self.direc_df.iat[index, 0], img0, self.direc_df.iat[index, 1], img1, dummy)
+
+    def __len__(self):
+        return len(self.direc_df)
+
+    def get_label_file(self):
+        print('label_file', self.label_path)
+        return self.label_path
+
+def data_loader(root, phase='train',  batch_size=64, repeat_num=10):
+    if phase == 'train':
+        shuffle = True
+    else:
+        shuffle = False
+    dataset = CustomDataset(root, phase,  repeat_num,
+                            transform=transforms.Compose([transforms.Resize((112,112)),
+                                                                      transforms.ToTensor(),
+                                                                      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                                                      ]))
+                            # transform=transforms.Compose([transforms.Resize((244)),
+                            #                                           transforms.ToTensor(),
+                            #                                           transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                            #                                           ]))
+    dataloader = data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
+    return dataloader, dataset.get_label_file()
+
+
